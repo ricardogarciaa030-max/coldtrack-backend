6867c0a9cf741374ff98b06ece984b2dc8261ab8
Ricardo Garcia <ricardogarciaa030@gmail.com>
2025-12-19 21:13:10 -0300

    Fix CORS: Allow Firebase Hosting domains

--- coldtrack/settings.py
+++ coldtrack/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:5173,http://localhost:3000'
+    default='http://localhost:5173,http://localhost:3000,https://carnes-car.web.app,https://carnes-car.firebaseapp.com'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
